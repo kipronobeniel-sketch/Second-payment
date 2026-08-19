4b0648d0f205f5cd40680dcfc5a5e02bd1d4e046
Kiprono Sigei <kipronobeniel@gmail.com>
2026-08-19 12:34:57 -0400

    My project

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
 print ("Hello")
 # Hello
-I go by thename Kiprono, on my way to becoming a software engineer.
+I go by the name Kiprono, on my way to becoming a software engineer.
  I am currently learning Python and working on various projects to enhance my skills.
